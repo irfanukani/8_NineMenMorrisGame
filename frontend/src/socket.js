3fcc2f167864f32f9c19388ac5d5398e07d23cde
Restyled.io <commits@restyled.io>
2023-12-03 09:37:45 +0000

    Restyled by clang-format

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 // socket.js
 
-import { io } from 'socket.io-client'
+import {io} from 'socket.io-client'
 
 const socket = io('https://8817-14-139-82-6.ngrok-free.app')
 
--- frontend/src/state/gameState.js
+++ frontend/src/state/gameState.js
@@ -1,10 +1,11 @@
-import { create } from 'zustand'
+import {create} from 'zustand'
 
-const useStore = create((set) => ({
-  currentUser: '',
-  gameBoard: new Array(24).fill(0),
-  setCurrentUser: (currentUser) => set({ currentUser: currentUser }),
-  setGameBoard: (gameBoard) => set({ gameBoard: gameBoard }),
-}))
+const useStore =
+    create((set) => ({
+             currentUser : '',
+             gameBoard : new Array(24).fill(0),
+             setCurrentUser : (currentUser) => set({currentUser : currentUser}),
+             setGameBoard : (gameBoard) => set({gameBoard : gameBoard}),
+           }))
 
 export default useStore
